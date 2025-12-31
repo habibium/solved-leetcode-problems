7c0a4b02681817a33dd5ef198ac6fbe7fa9aa4fd
habibium <habib.prodev@gmail.com>
2025-12-31 15:48:21 +0600

    add palindrome number solution in Rust

---
+++ 0041-palindrome-number.rs
@@ -0,0 +1,20 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        let mut remaining_digits = x;
+        let mut reversed = 0;
+
+        while remaining_digits != 0 {
+            reversed = reversed * 10 + remaining_digits % 10;
+            remaining_digits /= 10;
+        }
+
+        return x == reversed;
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::is_palindrome(121), true);
+    assert_eq!(Solution::is_palindrome(120), false);
+}

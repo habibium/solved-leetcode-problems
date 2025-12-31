9705df54fd6e17ee3fe7ea62c84b94c7005829d6
habibium <habib.prodev@gmail.com>
2025-12-31 15:56:28 +0600

    setup cargo

--- 0041-palindrome-number.rs
+++ 0041-palindrome-number.rs
@@ -2,6 +2,10 @@ struct Solution;
 
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+
         let mut remaining_digits = x;
         let mut reversed = 0;
 
@@ -17,4 +21,5 @@ impl Solution {
 fn main() {
     assert_eq!(Solution::is_palindrome(121), true);
     assert_eq!(Solution::is_palindrome(120), false);
+    println!("All tests passed!");
 }
